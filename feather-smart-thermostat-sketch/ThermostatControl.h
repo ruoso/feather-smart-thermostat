5820d9a8dcb5c34cda1d96ca2e05eca8212bfe0a
Daniel Ruoso <daniel@ruoso.com>
2020-10-17 17:53:01 -0400

    minor fix in current moving average

--- feather-smart-thermostat-sketch/ThermostatControl.h
+++ feather-smart-thermostat-sketch/ThermostatControl.h
@@ -22,8 +22,8 @@ namespace Thermostat {
   struct Sensors {
     Adafruit_SHT31 sht31;
     const static int current_a_pin = 39;
-    float moving_average[10];
-    int moving_average_count;
+    float moving_average[10] = {0,0,0,0,0,0,0,0,0,0};
+    int moving_average_index = 0;
     Sensors() {
     }
     void setup() {
@@ -39,17 +39,17 @@ namespace Thermostat {
       // These analog input pins have 12-bit resolution. This means that when you read an analog input, its range may vary from 0 to 4095.
       int measured = analogRead(current_a_pin);
       float normalized = round(((measured * 3.2)/4096)*10)/10;
-      float centered = normalized - 1.5;
+      float centered = normalized ;// - 1.5;
 
       // The CPU is already too busy for us to be able to read AC and calculate it correctly. So instead we'll just check that
-      // on average we have had a signal bigger than 0.1 over the past 10 measurements, which will most definitely be always true
+      // on average we have had an absolute signal bigger than 0.1 over the past 10 measurements, which will most definitely be always true
       // when we have current, and always false when we don't.
-      moving_average[(moving_average_count++)%10] = abs(centered);
+      moving_average[(moving_average_index++)%10] = abs(centered);
       float sum = 0;
-      for (int i = 0; i < 10 && i <= moving_average_count; i++) {
+      for (int i = 0; i < 10 && i <= moving_average_index; i++) {
         sum += moving_average[i];
       }
-      float avg = sum / ( moving_average_count > 10 ? 10 : moving_average_count + 1 );
+      float avg = sum / ( moving_average_index > 9 ? 10 : moving_average_index + 1 );
       if (avg > 0.1) {
         state.actual_current = 1;
       } else {
